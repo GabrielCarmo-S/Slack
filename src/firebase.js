5346f0f58387e548db861937dd63d9f783604389
GabrielCarmo-S <perien.op@gmail.com>
2021-03-31 11:56:31 -0300

    Init

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// export const firebaseConfig = {
+//   apiKey: 'AIzaSyDsl4CfMqRX9CSte5i1qdhBrufO0mzSKeY',
+//   authDomain: 'slack-clone-c94ec.firebaseapp.com',
+//   projectId: 'slack-clone-c94ec',
+//   storageBucket: 'slack-clone-c94ec.appspot.com',
+//   messagingSenderId: '1008035463540',
+//   appId: '1:1008035463540:web:8e2801da229470a55bac21',
+//   measurementId: 'G-SJ50QN4FCN',
+// };
