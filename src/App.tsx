433913198e1247d453a2a466337ac015e2f65690
GabrielCarmo-S <perien.op@gmail.com>
2021-03-31 21:05:10 -0300

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Header from './components/Header';
+
 function App() {
   return (
-  <h1>Slack</h1>
+    <div className="app">
+      <Router>
+        <>
+          <Switch>
+            <Route path="/" exact>
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>
+    </div>
   );
 }
 
